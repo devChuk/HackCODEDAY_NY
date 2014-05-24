c324b05db73acda8895ddb8b0722bab6b2a6a3f3
Brian Chuk <brian_chuk@yahoo.com>
2014-05-24 17:06:24 -0400

    blah

--- Ball.java
+++ Ball.java
@@ -3,7 +3,55 @@ import java.io;
 import java.net.*
 
 public class Ball {
+	private int x;
+	private int y;
+	private int xVel;
+	private int yVel;
+	private int wang; //It's an inside joke. This is meant to slow down velocity. The larger the wang, the faster it slows down.
 
-	
+	public Ball(int x, int y) {
+		setX(x);
+		setY(y);
+		setXvel(0);
+		setYvel(0);
 
+	}
+	public int setX(int hurr) {
+		x = hurr;
+		return x;
+	}
+	public int setY(int hurr) {
+		y = hurr;
+		return y;
+	}
+	public int getX() {
+		return x;
+	}
+	public int getY() {
+		return x;
+	}
+	public int getXvel() {
+		return xVel;
+	}
+	public int getYvel() {
+		return yVel;
+	}
+	public void setXvel(int v) {
+		xVel = v;
+	}
+	public void setYvel(int v) {
+		yVel = v;
+	}
+	public void move() {
+		x += xVel;
+		y += yVel;
+	}
+	public slowdown() {
+		if (xVel >= wang) {
+			xVel -= wang;
+		}
+		if (yVel >= wang) {
+			yVel -= wang;
+		}
+	}
 }
