224aed3f23fa579e4b3f22bbec063fd000f445cc
Patrick Tsai <patosai@live.com>
2014-05-24 17:47:28 -0400

    Added mouse listener skeleton

--- Pool.java
+++ Pool.java
@@ -1,13 +1,28 @@
-import javax.swing;
-import java.io;
+import javax.swing.*;
+import java.io.*;
 import java.net.*;
 import java.util.*;
+import java.awt.event.*;
 
-public class Pool {
-	private ArrayList<bal
+public class Pool extends JFrame{
+	private ArrayList<Ball> balls = new ArrayList<Ball>();
 	private int winner = 0; //0 when nobody has won yet; 1 if player 1 wins, 2 if player 2 wins.
 
 	public Pool() {
-
+		setTitle("Pool");
+		setSize(500, 500);
+	}
+	
+	class MouseListener extends MouseAdapter {
+		public void mousePressed(MouseEvent e) {
+		
+		}
+		
+		public void mouseClicked(MouseEvent e) {
+		
+		}
+		public void mouseReleased(MouseEvent e) {
+		
+		}
 	}
 }
