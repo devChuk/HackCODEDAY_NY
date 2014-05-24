dac96de68e2c5760ebf78eb35df0af81dd600449
Brian Chuk <brian_chuk@yahoo.com>
2014-05-24 15:39:22 -0400

    created constructor header

--- Player.java
+++ Player.java
@@ -1,3 +1,21 @@
 import java.util.*;
 import java.io.*;
 import javax.swing.*;
+
+public class Player {
+	protected int x;
+    protected int y;
+    protected int width;
+    protected int height;
+
+    public Player() {}
+
+
+
+
+
+
+
+
+
+}
