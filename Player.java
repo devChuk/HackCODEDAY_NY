df5928b5af1aab6ca18caec645f7b6c5f97608d5
Brian Chuk <brian_chuk@yahoo.com>
2014-05-24 15:41:35 -0400

    made get func

--- Player.java
+++ Player.java
@@ -8,9 +8,19 @@ public class Player {
     protected int width;
     protected int height;
 
-    public Player() {}
+    public Player(int width, int height) {
+    	
+    }
 
+    public int getX()
+    {
+	return this.x;
+    }
 
+    public int getY()
+    {
+	return this.y;
+    }
 
 
 
