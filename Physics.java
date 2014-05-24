3c2db4db12decf840cb3f413f25a70261214fba3
Patrick Tsai <patosai@live.com>
2014-05-24 16:23:10 -0400

    Added elementary collision detection

--- Physics.java
+++ Physics.java
@@ -1,5 +1,13 @@
 // This is where all the heavy duty computing is!
 
 public class Physics {
+	private final int BALL_SIZE = 30;
 
+	public boolean willItCollide(Ball a, Ball b) {
+		int dx = Math.abs( a.getX() - a.getY() );
+		int dy = Math.abs( a.getY() - a.getY() );
+		
+		if (Math.sqrt(dx * dx + dy * dy) < BALL_SIZE) return true;
+		return false;
+	}
 }
