29ca63b9760cce48bcaa9292bf45120a364aaf2b
Patrick Tsai <patosai@live.com>
2014-05-24 15:37:30 -0400

    Started main class

--- CaptureTheCastle.java
+++ CaptureTheCastle.java
@@ -1,7 +1,29 @@
-public class CaptureTheCastle {
+import java.awt.event.*;
+import java.io.*;
+import java.util.*;
+import javax.swing.*;
+
+private boolean up, down, left, right;
+
+public class CaptureTheCastle extends JFrame {
 	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+	}
 }
 
+
+
 class ArrowListener implements KeyListener {
-	
+	public void keyPressed(KeyEvent e) {
+		
+	}
+	public void keyTyped(KeyEvent e) { }
+	public void keyReleased(KeyEvent e) {
+		
+	}
 }
