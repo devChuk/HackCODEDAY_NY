e3273746ffba8bab89c0105730c453969a44013d
Ida <ihuangda@gmail.com>
2014-05-24 19:36:00 -0400

    direction

--- CueBall.java
+++ CueBall.java
@@ -1,15 +1,17 @@
-/*
-  class CueBall 
- */
+//class CueBall
 
 public class CueBall extends Ball {
 
-	public CueBall(int x, int y) {
-		super(0, x, y);
-	}
+    //instance vars
+    private int direction;
 
-
-
-
+    //constructor
+    public CueBall(int x, int y) {
+	super(0, x, y);
+    }
+    
+    public int setDirection(int dir){
+	direction = dir;
+    }
 
 } // end class Cue Ball
